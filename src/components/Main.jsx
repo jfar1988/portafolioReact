dc550b6a2571d1619b3649e0cc22bc67e16712ae
jfar1988 <jfar1988@misena.edu.co>
2023-04-02 21:27:54 -0500

    portafolio made in React

---
+++ src/components/Main.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import {
+  FaTwitter,
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedin,
+} from "react-icons/fa";
+
+const Main = () => {
+  return (
+    <div id="main">
+      <img
+        className="w-full h-screen object-cover object-left "
+        src="https://images.unsplash.com/photo-1625472603513-413fb94f8b0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        alt="image-bg"
+      />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center 2xl:items-start items-center text-center">
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800 ">
+            I'm Juan Fdo Aguilar R.
+          </h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
+            I'm a
+            <TypeAnimation
+              sequence={[
+                "Developer",
+                1000,
+                "Coder",
+                2000,
+                "Tech Enthusuast",
+                2000,
+                "Disciplined Person",
+                2000,
+              ]}
+              wrapper="span"
+              cursor={true}
+              repeat={Infinity}
+              style={{
+                fontSize: "1em",
+                paddingLeft: "5px",
+              }}
+            />
+          </h2>
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
+            <FaTwitter className="cursor-pointer" size={20} />
+            <FaFacebookF className="cursor-pointer" size={20} />
+            <FaInstagram className="cursor-pointer" size={20} />
+            <FaLinkedin className="cursor-pointer" size={20} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/components/Work.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Workitem from "./Workitem";
+
+const data = [
+  {
+    year: 2018,
+    title: "Profesor de física(Publico)",
+    duration: "4 Years",
+    details:
+      "Atender las asignaturas de física y física de laboratorio en grados decimos, además, atender las funciones referentes a semilleros para estudiantes con dificultades en la asignatura.",
+  },
+  {
+    year: 2016,
+    title: "Profesor de Matemáticas(Privado)",
+    duration: "2 Years",
+    details:
+      "Atender las asignaturas de matemáticas y estadística en grados tercero hasta séptimo, además, funciones referentes al facilitar la gestión de calidad en el área de proyección humanística y asesorar proyecto de investigación de las estudiantes de undécimo",
+  },
+];
+
+const Work = () => {
+  return (
+    <div id="work" className="max-w-[1366] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">Work</h1>
+      {data.map((item, idx) => (
+        <Workitem
+          key={idx}
+          year={item.year}
+          title={item.title}
+          duration={item.duration}
+          details={item.details}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Work;
